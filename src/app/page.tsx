2d6844c682022cad4ad535a93e8c4b973a42734c
kimsuji <skme1103@naver.com>
2024-01-31 22:46:54 +0900

    240131_2024포폴_페이지 폴더링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ function Page() {
     return (
       <div className="App">
           <p>
-           page Edit <code>src/App.tsx</code> and save to reload.
+           home page
           </p>
       </div>
     );
